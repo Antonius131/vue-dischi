e26e269a11587aa4892f14e4b27f03f935a2f69e
Antonius131 <antomendetta@gmail.com>
2022-07-19 15:14:13 +0200

    MainContent: create html content structure

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,7 +1,17 @@
 <template>
-  <main>
-
-  </main>
+   <main>
+      <div class="wrapper">
+         <div class="card">
+            <div class="card-img">
+               <img src="https://cloud.rtl.it/RTLFM/News/Article/600x600/buon-compleanno-freddie-mercury-avrebbe-75-anni-quel-giorno-al-live-aid-in-cui-fu-incoronato-re-square-site-tv25s.jpg" alt="freddie">
+            </div>
+            <div class="card-caption">
+               <h4>Title</h4>
+               <h6>Subtitle</h6>
+            </div>
+         </div>
+      </div>
+   </main>
 </template>
 
 <script>
