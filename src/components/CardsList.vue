cc279054bbc6fb235eee7cdbbd29b3dd28def37f
Antonius131 <antomendetta@gmail.com>
2022-07-19 16:20:45 +0200

    CardsList: get objects array from URL

--- src/components/CardsList.vue
+++ src/components/CardsList.vue
@@ -1,15 +1,32 @@
 <template>
-  <div class="card-wrapper">
+   <div class="card-wrapper">
       <ArtistCard />
    </div>
 </template>
 
 <script>
 import ArtistCard from './ArtistCard.vue';
+import axios from 'axios';
 
 export default {
    components: { 
       ArtistCard,
+   },
+   data: function() {
+      return {
+         artistCard: []
+      }
+   },
+   methods: {
+      getArtistCard() {
+         axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+         .then((result) => {
+            console.log(result.data.response);
+         });
+      }
+   },
+   created() {
+      this.getArtistCard();
    }
 }
 </script>
