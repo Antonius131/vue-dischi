5f77d1e216d79961af8918ea22cf2096c218cfbf
Antonius131 <antomendetta@gmail.com>
2022-07-19 15:43:09 +0200

    CardsList: define HTML structure and SASS style

--- src/components/CardsList.vue
+++ src/components/CardsList.vue
@@ -1,5 +1,15 @@
 <template>
-  <div></div>
+  <div class="card">
+      <div class="card-content">
+         <div class="card-img">
+            <img src="https://cloud.rtl.it/RTLFM/News/Article/600x600/buon-compleanno-freddie-mercury-avrebbe-75-anni-quel-giorno-al-live-aid-in-cui-fu-incoronato-re-square-site-tv25s.jpg" alt="freddie">
+         </div>
+         <div class="card-caption">
+            <h3>Title</h3>
+            <p>Subtitle</p>
+         </div>
+      </div>
+   </div>
 </template>
 
 <script>
@@ -8,6 +18,31 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+   .card {
+      width: calc(100% / 5);
+      padding: 0.5rem 1rem;
 
+      .card-content {
+         background-color: rgba(255,255,255,.05);
+         padding: 1rem;
+         color: #fff
+      }
+
+      img {
+         width: 100%;
+      }
+
+      .card-caption {
+         padding: .5rem 0;
+
+         h3 {
+            text-transform: uppercase;
+         }
+
+         p {
+            color: rgba(255,255,255,.5);
+         }
+      }
+   }
 </style>
--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -2,17 +2,6 @@
    <main>
       <div class="wrapper">
          <CardsList />
-         <!-- <div class="card">
-            <div class="card-content">
-               <div class="card-img">
-                  <img src="https://cloud.rtl.it/RTLFM/News/Article/600x600/buon-compleanno-freddie-mercury-avrebbe-75-anni-quel-giorno-al-live-aid-in-cui-fu-incoronato-re-square-site-tv25s.jpg" alt="freddie">
-               </div>
-               <div class="card-caption">
-                  <h3>Title</h3>
-                  <p>Subtitle</p>
-               </div>
-            </div>
-         </div> -->
       </div>
    </main>
 </template>
@@ -37,35 +26,6 @@ export default {
          flex-wrap: wrap;
          width: 70%;
          margin: 0 auto;
-
-         
-
-         .card {
-            width: calc(100% / 5);
-            padding: 0.5rem 1rem;
-
-            .card-content {
-               background-color: rgba(255,255,255,.05);
-               padding: 1rem;
-               color: #fff
-            }
-
-            img {
-               width: 100%;
-            }
-
-            .card-caption {
-               padding: .5rem 0;
-
-               h3 {
-                  text-transform: uppercase;
-               }
-
-               p {
-                  color: rgba(255,255,255,.5);
-               }
-            }
-         }
       }
    }
 </style>
