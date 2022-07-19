db6a355b11bf33e28c68c4f642f11a9d78501fcb
Antonius131 <antomendetta@gmail.com>
2022-07-19 15:59:14 +0200

    ArtistCard: create and import ArtistCard component

--- src/components/CardsList.vue
+++ src/components/CardsList.vue
@@ -1,22 +1,16 @@
 <template>
   <div class="card-wrapper">
-      <div class="card">
-         <div class="card-content">
-            <div class="card-img">
-               <img src="https://cloud.rtl.it/RTLFM/News/Article/600x600/buon-compleanno-freddie-mercury-avrebbe-75-anni-quel-giorno-al-live-aid-in-cui-fu-incoronato-re-square-site-tv25s.jpg" alt="freddie">
-            </div>
-            <div class="card-caption">
-               <h3>Title</h3>
-               <p>Subtitle</p>
-            </div>
-         </div>
-      </div>
+      <ArtistCard />
    </div>
 </template>
 
 <script>
-export default {
+import ArtistCard from './ArtistCard.vue';
 
+export default {
+   components: { 
+      ArtistCard,
+   }
 }
 </script>
 
@@ -27,32 +21,6 @@ export default {
       flex-wrap: wrap;
       width: 70%;
       margin: 0 auto;
-
-      .card {
-      width: calc(100% / 5);
-      padding: 0.5rem 1rem;
-
-         .card-content {
-            background-color: rgba(255,255,255,.05);
-            padding: 1rem;
-            color: #fff
-         }
-
-         img {
-            width: 100%;
-         }
-
-         .card-caption {
-            padding: .5rem 0;
-
-            h3 {
-               text-transform: uppercase;
-            }
-
-            p {
-               color: rgba(255,255,255,.5);
-            }
-         }
-      }
    }
+
 </style>
