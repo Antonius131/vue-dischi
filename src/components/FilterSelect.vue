6f3d322eb798313a903dd032d7204140905711ee
Antonius131 <antomendetta@gmail.com>
2022-07-20 15:41:28 +0200

    FilterSelect: put selectedValue in select

--- src/components/FilterSelect.vue
+++ src/components/FilterSelect.vue
@@ -1,23 +1,33 @@
 <template>
    <div id="cards-filter">
-      <select>
-         <option selected></option>
-         <option value="1">Genre</option>
-         <option value="2">Artist</option>
+      <select v-model="selectedValue">
+         <option></option>
+         <option>Genre</option>
+         <option>Artist</option>
       </select>
    </div>
 </template>
 
 <script>
 export default {
-
+   data: function() {
+      return {
+         selectedValue: ''
+      }
+   }
 }
 </script>
 
 <style lang="scss">
    #cards-filter {
-   width: 100%;
-   text-align: left;
-   margin: 0rem 1rem 1rem 1rem;
+      width: 100%;
+      text-align: left;
+      margin: 0rem 1rem 1rem 1rem;
+
+      select {
+         background: #fff;
+         border: 1px solid #fff;
+         padding: .25rem 1rem;
+      }
    }
 </style>
