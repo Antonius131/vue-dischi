f734fa7ff94ea8f3c7ba46187d90a4f4e671ac00
Antonius131 <antomendetta@gmail.com>
2022-07-20 15:06:36 +0200

    create ArtistCard and put it in CardList

---
+++ src/components/ArtistCard.vue
@@ -0,0 +1,45 @@
+<template>
+   <div class="card">
+      <div class="card-img">
+         <img src="poster" alt="author">
+      </div>
+      <div class="card-caption">
+         <h4>title</h4>
+         <p>author</p>
+         <p>year</p>
+      </div>
+   </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+   .card {
+      width: calc(100% / 5);
+      height: 300px;
+      background-color: rgba(255,255,255,.05);
+      padding: 1rem;
+      color: #fff;
+
+         img {
+            width: 100%;
+         }
+
+         .card-caption {
+            padding: .5rem 0;
+
+            h4 {
+               text-transform: uppercase;
+               margin-bottom: .75rem;
+            }
+
+            p {
+               color: rgba(255,255,255,.3);
+            }
+         }
+   }
+</style>
--- src/components/CardsList.vue
+++ src/components/CardsList.vue
@@ -1,38 +1,33 @@
 <template>
    <div class="card-wrapper">
-      <div class="card" v-for="(artist, index) in artistCard" :key="index">
-         <div class="card-img">
-            <img :src="artist.poster" :alt="artist.author">
-         </div>
-         <div class="card-caption">
-            <h4>{{ artist.title }}</h4>
-            <p>{{ artist.author }}</p>
-            <p>{{ artist.year }}</p>
-         </div>
-      </div>
+      <ArtistCard />
    </div>
 </template>
 
 <script>
 import axios from 'axios';
+import ArtistCard from './ArtistCard.vue';
 
 export default {
-   data: function() {
+   data: function () {
       return {
          artistCard: []
-      }
+      };
    },
    methods: {
       getArtistCard() {
-         axios.get('https://flynn.boolean.careers/exercises/api/array/music')
-         .then((result) => {
-            this.artistCard = result.data.response; 
-            console.log(this.artistCard);
+         axios.get("https://flynn.boolean.careers/exercises/api/array/music")
+               .then((result) => {
+               this.artistCard = result.data.response;
+               console.log(this.artistCard);
          });
       }
    },
    created() {
       this.getArtistCard();
+   },
+   components: { 
+   ArtistCard 
    }
 }
 </script>
@@ -44,31 +39,6 @@ export default {
       flex-wrap: wrap;
       width: 70%;
       margin: 0 auto;
-
-      .card {
-      width: calc(100% / 5);
-      height: 300px;
-      background-color: rgba(255,255,255,.05);
-      padding: 1rem;
-      color: #fff;
-
-         img {
-            width: 100%;
-         }
-
-         .card-caption {
-            padding: .5rem 0;
-
-            h4 {
-               text-transform: uppercase;
-               margin-bottom: .75rem;
-            }
-
-            p {
-               color: rgba(255,255,255,.3);
-            }
-         }
-      }
    }
 
 </style>
